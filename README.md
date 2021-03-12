83ee2c5d2b16060ff18a2bd429d3980d7f16a64d
sofianezerrouki <sofianeyooung@gmail.com>
2021-03-12 20:37:13 +0100

    initialisation of project

---
+++ README.md
@@ -0,0 +1,4 @@
+# E-commerce with spring & angular
+
+#front End  
+[ecommerce_spring_angular_frontend](https://github.com/sofianezerrouki/ecommerce_spring_angular_frontend.git "Front End with Angular")
---
+++ src/test/java/com/camacode/ecommerce/EcommerceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camacode.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
