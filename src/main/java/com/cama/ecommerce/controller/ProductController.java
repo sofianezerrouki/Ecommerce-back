4e4066a44a57033ea03e842865b7d3100afac704
sofianezerrouki <sofianeyooung@gmail.com>
2021-03-15 22:07:01 +0100

    change Product Controller

--- src/main/java/com/cama/ecommerce/controller/ProductController.java
+++ src/main/java/com/cama/ecommerce/controller/ProductController.java
@@ -1,17 +1,23 @@
 package com.cama.ecommerce.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/products/")
 public class ProductController {
 	
 	@GetMapping
 	public String getProduct() {
 		
 		return "ProductController Work :) ";
+	}
+	@GetMapping("{theId}")
+	public String getProduct(@PathVariable long theId) {
+		
+		return "Product : "+theId+" for 10$ :) lol";
 	}
 
 }
