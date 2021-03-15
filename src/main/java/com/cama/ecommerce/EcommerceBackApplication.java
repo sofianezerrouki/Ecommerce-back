a0e47912ebe420136f50e4f4309307af07fc1420
sofianezerrouki <sofianeyooung@gmail.com>
2021-03-15 22:04:09 +0100

    add Product Controller

--- src/main/java/com/cama/ecommerce/EcommerceBackApplication.java
+++ src/main/java/com/cama/ecommerce/EcommerceBackApplication.java
@@ -1,4 +1,4 @@
-package com.camacode.ecommerce;
+package com.cama.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
